671f4e254f4ce69b02e5e57067d6b26aeb836424
fmastroleo <mastroleo.federico@mayo.edu>
2025-09-15 21:07:10 -0500

    Update lastRevised date in hura-imaging.ts
    Updated date of review
    
    Signed-off-by: fmastroleo <mastroleo.federico@mayo.edu>

--- src/data/products/auto-contouring/hura-imaging.ts
+++ src/data/products/auto-contouring/hura-imaging.ts
@@ -89,6 +89,6 @@ export const HURA_IMAGING_PRODUCTS: ProductDetails[] = [
     version: "2.5",
     releaseDate: "2023-05-20",
     lastUpdated: "2024-01-15",
-    lastRevised: "2025-09-01"
+    lastRevised: "2025-09-15"
   }
 ];
